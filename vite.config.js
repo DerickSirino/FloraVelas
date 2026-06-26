171d275bb43cd4dbded30375b0aafa848c290889
dericksirino <dericksirino@Mac-mini-de-Derick.local>
2026-06-25 22:25:59 -0300

    Configurando build para implantação no github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/FloraVelas/', // Altere para o nome do seu repositório no GitHub
+  build: {
+    outDir: 'docs',
+  },
   server: {
     allowedHosts: [
       'localhost',
